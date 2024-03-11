334b30669783749324baef5f6979697b26cd7c5e
siweli <lewisr.py@gmail.com>
2024-03-11 14:33:02 +0000

    created account page

---
+++ src/app/fit-gym/account/page.tsx
@@ -0,0 +1,10 @@
+import "../globals.css";
+
+export default function Home() {
+    return (
+        <main>
+            <title>Account</title>
+            <h1>test 3</h1>
+        </main>
+    )
+}
